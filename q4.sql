1c1b997743e2cdbee04e949488beeb664b439cc7
Dasean Volk <80739088+Cyber-Dust@users.noreply.github.com>
2022-09-16 23:52:00 -0700

    Update q4.sql

--- q4.sql
+++ q4.sql
@@ -1,3 +1,5 @@
+-- How to Create Table & Insert Data into a simple oracle RDB
+
 CREATE TABLE courses (course_id INT primary key, title varchar(225), semester varchar(20));
 insert into courses values (3301, 'DSCI551', 'FALL 2022');
 insert into courses values (3302, 'BIO247', 'SPRING 2022');
@@ -17,5 +19,5 @@ insert into student values (1103, 'Tom Riddle', 3.9);
 CREATE TABLE teaching_assistant (student_id INT primary key, name VARCHAR(30), gpa REAL, hourly_rate DECIMAL (4,2)NOT NULL);
 -- Subclass entity
 insert into teaching_assistant  values (1104, 'Jennifer Goodtime', 4.0, 17.00);
-insert into teaching_assistant  values (1105, 'Liam Hennessy', 3.1, 15.00);
+insert into teaching_assistant  values (1105, 'Liam Hendrix', 3.1, 15.00);
 insert into teaching_assistant  values (1106, 'Michael Scott', 3.5, 10.00);
